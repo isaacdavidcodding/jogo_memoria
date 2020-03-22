26fb6db7c706834b98d3ac03fc9e23837e9bb7d5
Isaac <isaacdavid797@gmail.com>
2020-03-22 20:29:27 -0300

    Apagando lógica de métodos antes comentados para inicar reimplementação.

--- controlador.js
+++ controlador.js
@@ -1,64 +1,10 @@
 const controlador = {
-    // Variaveis
     
+    adicionarEventoClique: () => { 
+        let lista = document.querySelectorAll('.cartaMemoria')
+        console.log(lista)
 
-    /*
-    (function shuffle() {
-        cards.forEach(card => {
-            let randomPos = Math.floor(Math.random() * 12)
-            card.style.order = randomPos
-        })
-    })(), */
-
-    // Funcoes
-   /*  desvirarCartas: () => {
-        lockBoard = true
-        setTimeout(() => {
-            primeiraCarta.classList.remove('flip')
-            segundaCarta.classList.remove('flip')
-            resetarQuadro()
-        }, 1500)
-    },
-
-    desabilitarCartas: () => {
-        primeiraCarta.removeEventListener('click', virarCarta)
-        segundaCarta.removeEventListener('click', virarCarta)
-        resetarQuadro()
-    },
-
-    checarCombinacao: () => {
-        let isMatch = primeiraCarta.dataset.framework === segundaCarta.dataset.framework
-
-        isMatch ? desabilitarCartas() : unflipCards()
-    },
-
-    resetarQuadro: () => {
-        [temCartaVirada, quadroTravado] = [false, false]
-        [primeiraCarta, segundaCarta] = [null, null]
-    }, */
-
-    virarCarta: () => {
-        /* if (this.quadroTravado) return
-
-        if (this === this.primeiraCarta) return   
-
-        // this.classList.add('flip')
-
-        if (!this.temCartaVirada) {
-            this.temCartaVirada = true
-            this.primeiraCarta = this
-
-            return
-        } 
-
-        this.segundaCarta = this
-        checarCombinacao() */
-
-    },
-    
-   /*  adicionarEventoClique: (cartas, virarCarta) => { 
-        cartas.forEach(carta => carta.addEventListener('click', virarCarta))
-    }, */
+    }, 
     
     render: (template, value) => { return Mustache.render(template, value) },
 
@@ -91,6 +37,6 @@ const controlador = {
 
     iniciarControle: function() {
         this.carregarFigurinhas()
-        // this.adicionarEventoClique(this.cartas, this.virarCarta.bind(controlador))
+        this.adicionarEventoClique()
     }
 }   
